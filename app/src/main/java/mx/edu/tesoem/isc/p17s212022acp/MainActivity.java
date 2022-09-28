558bff7442ce63b3baae9c8e24aee615ecc76db1
abraham317 <abrahamcontrerasperez317@gmail.com>
2022-09-28 10:16:15 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p17s212022acp/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p17s212022acp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
